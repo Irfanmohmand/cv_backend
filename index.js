51fcf79d9444f5bbb51a673b677dbf80dc0b60fa
Irfan Mohmand <irfanmohmand987@gmail.com>
2025-07-25 12:00:42 +0500

    something added for vercel deployment

---
+++ index.js
@@ -0,0 +1,27 @@
+// backend/api/user.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connection.js";
+import userRoute from "./routes/userRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+app.use(express.json());
+
+connectDB();
+app.use('/api/v1/user', userRoute);
+
+
+const PORT = 4002 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+
+})
